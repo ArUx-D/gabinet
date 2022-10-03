631c4bfe496c7b9a99bd22efb740cd7538f78adb
Natalia <natalia.krysaa@gmail.com>
2022-10-03 11:36:27 +0200

    working menu

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import React from 'react';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Menu, MenuItem, MenuButton } from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
+import '@szhsin/react-menu/dist/transitions/slide.css';
 import './style.css';
 import Office from './pages/Office';
 import Clients from './pages/Clients';
 import Visits from './pages/Visits';
 import Error from './pages/Error';
+import Add from './pages/client/Add';
+import All from './pages/client/All';
 function App() {
   let activeClassName = "nav-active";
-  return (
-    <BrowserRouter>
-      <header>
-        <h1>Gabinet</h1>
-      </header>
-      <nav>
-        <NavLink end to="/" className={({ isActive }) => isActive ? activeClassName : undefined}>Gabinet</NavLink>
-        <NavLink to="clients" className={({ isActive }) => isActive ? activeClassName : undefined}>Klienci</NavLink>
-        <NavLink to="visits" className={({ isActive }) => isActive ? activeClassName : undefined}>Wizyty</NavLink>
-      </nav>
+    return (
+      <BrowserRouter>
+    <Menu menuButton={<MenuButton>Klienci</MenuButton>}>
+      <MenuItem href="/clients/add" >Nowy klient</MenuItem>
+      <MenuItem href="/clients/all">Lista klientów</MenuItem>
+      <MenuItem>Close Window</MenuItem>
+    </Menu>    
+    <Menu menuButton={<MenuButton>Klienci</MenuButton>}>
+      <MenuItem href="/clients/add" >Nowy klient</MenuItem>
+      <MenuItem href="/clients/all">Lista klientów</MenuItem>
+      <MenuItem>Close Window</MenuItem>
+    </Menu>    
+    <Menu menuButton={<MenuButton>Klienci</MenuButton>}>
+      <MenuItem href="/clients/add" >Nowy klient</MenuItem>
+      <MenuItem href="/clients/all">Lista klientów</MenuItem>
+      <MenuItem>Close Window</MenuItem>
+    </Menu>
       <Routes>
         <Route path="/" element={<Office/>}/>
         <Route path="clients" element={<Clients/>}/>
         <Route path="visits" element={<Visits/>}/>
         <Route path="*" element={<Error/>}/>
+        <Route path="clients/add" element={<Add/>}/>
+        <Route path="clients/all" element={<All/>}/>
       </Routes>
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import Add from './client/Add'
 import All from './client/All'
 import Error from './Error'
@@ -8,20 +7,10 @@ import '../style.css'
 function Clients(){
   let activeClassName = "nav-active";
   return (
-    <BrowserRouter>
       <div>
         <p>This is the Clients Page</p>
     </div>
-      <nav>
-        <NavLink end to="add" className={({ isActive }) => isActive ? activeClassName : undefined}>Dodaj klienta</NavLink>
-        <NavLink to="all" className={({ isActive }) => isActive ? activeClassName : undefined}>Wszyscy klienci</NavLink>
-      </nav>
-      <Routes>
-        <Route path="add" element={<Add/>}/>
-        <Route path="all" element={<All/>}/>
-        <Route path="*" element={<Error/>}/>
-      </Routes>
-    </BrowserRouter>
+
 
   )
 }
